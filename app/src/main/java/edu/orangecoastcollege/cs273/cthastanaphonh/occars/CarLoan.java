36d149dae89ca8c9a98c98458c5a9b0296253c96
Edgar <mendoza.edgar@csu.fullerton.edu>
2017-10-31 01:16:08 -0700

    Adding JavaDocs to methods in CarLoan Class

--- app/src/main/java/edu/orangecoastcollege/cs273/cthastanaphonh/occars/CarLoan.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/cthastanaphonh/occars/CarLoan.java
@@ -11,28 +11,39 @@ public class CarLoan {
     private double mDownPayment;
     private int mTerm;
 
-
-
-
-
-
-
+/**
+* This method returns the borrowed amount, as a double, by 
+* subtracting the price from the down payment. 
+*/
     public double borrowedAmount()
     {
         return mPrice - mDownPayment;
     }
 
+/**
+* This method returns the tax amount as a double
+* by multiplying the price by the sales tax percentage 
+*/
     public double taxAmount()
     {
         return mPrice * STATE_TAX;
     }
-
+    
+/**
+* This method returns the total amount as a double
+* by adding the price to the tax amount 
+*/
     public double totalAmount()
     {
         return mPrice + taxAmount();
     }
 
-
+/**
+* This method returns the interest amount as a double
+* by first determining which term was selected and 
+* multiplying the borrowed amount to the interest rate
+* based on the term selected. 
+*/
     public double interestAmount()
     {
         double interestRate;
@@ -60,7 +71,9 @@ public class CarLoan {
         return borrowedAmount() * interestRate;
 
     }
-
+/**
+* This method returns the monthly payments as a double
+*/
     public double monthlyPayment()
     {
         return (borrowedAmount() + interestAmount()) / (mTerm * 12);
